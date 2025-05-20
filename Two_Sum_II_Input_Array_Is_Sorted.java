02aef0133e76c9f1a129613d8c1d29f3f81d5b5c
Anurag Singh <silentk13.10@gmail.com>
2025-05-20 23:27:08 +0530

    Add files via upload

---
+++ Two_Sum_II_Input_Array_Is_Sorted.java
@@ -0,0 +1,19 @@
+public class Two_Sum_II_Input_Array_Is_Sorted {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{};
+    }
+}
